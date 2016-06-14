bc48084dc140f40dd822fc588af095c8422dd8b7
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-06-14 16:52:56 +0530

    Added a default static directory.

--- urlscript/settings.py
+++ urlscript/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # The path where the bwrap executable is located
 BUBBLEWRAP_PATH = ""
